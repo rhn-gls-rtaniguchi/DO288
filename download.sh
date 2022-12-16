ad872b84f3cdeca2ade08f7ff41418e7303f55ea
Student User <student@workstation.lab.example.com>
2022-12-16 13:48:36 -0500

    Fixed contents

--- download.sh
+++ download.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 sudo wget -O /usr/bin/helm https://mirror.openshift.com/pub/openshift-v4/clients/helm/latest/helm-linux-amd64
-helm completion bash > helm_completion
+sudo chmod 755 /usr/bin/helm
+sudo helm completion bash > helm_completion
 sudo mv helm_completion /etc/bash_completion.d/helm
 source /etc/bash_completion.d/helm
 
